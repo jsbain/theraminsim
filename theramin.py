234ec5a3b67b45b4b1fa727c99560e552ef67fc8
jsbain <jsbain@yahoo.com>
2015-04-23 09:10:52 +0000

    Initial commit

--- theramin.py
+++ theramin.py
@@ -9,77 +9,54 @@ Eventually: multitouch
 Inspired by http://ptheremin.sourceforge.net, but completely rewritten for numpy/pythonista
 """
 
-import numpy as np
+import numpy
 import threading
 import time
 import wave
 import tempfile
-
-SCALES = ("chromatic", "diatonic major", "pentatonic major", "pentatonic minor", "blues")
-INIT_FREQ = 20
-
-NAME="PTheremin"
-VERSION="0.2.1"
-
-
-# from "Musical Instrument Design" by Bart Hopkin
-sharp = 1.05946
-equal_temp_freqs = [16.352, 16.352*sharp, 18.354, 18.354*sharp, 20.602, 21.827, 21.827*sharp, 24.500, 24.500*sharp, 27.500, 27.500*sharp, 30.868]
-equal_temp_labels = ['C*', 'C*#', 'D*', 'D*#', 'E*', 'F*', 'F*#', 'G*', 'G*#', 'A*', 'A*#', 'B*']
-
-equal_temp_tuning = zip(equal_temp_labels, equal_temp_freqs)
-
-diatonic_major_intervals = (0, 2, 4, 5, 7, 9, 11)
-pentatonic_major_intervals = (0, 2, 4, 7, 9)
-pentatonic_minor_intervals = (0, 3, 5, 7, 10)
-blues_intervals = (0, 3, 5, 6, 7, 10)
-
-# build up several octaves of notes
-NOTES = []
-for octave in range(11):
-    for label,freq in equal_temp_tuning:
-        NOTES.append((label.replace('*', "%d" % octave), (2**octave)*freq))
-
-def just_freqs(notes):
-    return [freq for label,freq in notes]
+import sound
+#set min and max frequencies (hz)
+fmin=150.0
+fmax=1000.0
 
 class PlaybackThread(threading.Thread):
     """A thread that continually generates audio."""
 
     def __init__(self, name, dt):
-        self.name = name
+        #self.name = name
 
-        self.fs = 8000.0 # the sample frequency
-        self.ft = INIT_FREQ # the base frequency of the instrument
-        self.vol = 1
+        self.fs = 4000.0 # the sample frequency
+        self.ft = fmin # the tone frequency of the instrument
+        self.vol = 1  #0-1
 
         # setup ping pong file/players
-        self.filelist = [tempfile.NamedTemporaryFile(delete=False),
-                         tempfile.NamedTemporaryFile(delete=False)]
+        self.filelist = [tempfile.NamedTemporaryFile(),
+                         tempfile.NamedTemporaryFile()]
         #
-        self.paused = True
-        self.alive = True
-        
+        self.alive = True    
+
+        self.dt = dt #update interval
+        self.idx = 0 # which buffer are we writing to
+        self.t = numpy.linspace(0,self.dt,np.round(self.dt*self.fs))  
         threading.Thread.__init__(self, name=name)
-        self.dt = 0.1
-        self.idx = 0
-        self.t = numpy.linspace(0,self.dt,np.round(self.dt*self.fs))
+        
     def run(self):
         def tone_gen():            
-            """Generate approximately dt's worth of tone.  Start/stop when signal is near zero, to avoid glitches."""
+            """Generate approximately dt's worth of tone.  Start/stop when signal is near zero, to avoid glitches.  this doesnt really work"""
             ft = self.ft
             dt = self.dt
             fs = self.fs
             twopift = 2*numpy.pi*ft
             sin = numpy.sin
-            
+            floor=numpy.floor
             vol = self.vol
-            numCycles = floor(ft.*dt)
+            numCycles = floor(ft*dt)
             numSamples = floor(numCycles/ft*fs)
             return  ( 127 + 128*0.95*vol * sin(twopift*self.t[0:numSamples])).astype('u1').tostring()
 
         def gen_file(file):
             tone=tone_gen()
+            file.seek(0,0)
             wf=wave.open(file,'w')
             wf.setparams((1, 1, self.fs, 0, 'NONE', 'not compressed'))
             wf.writeframes(tone)
@@ -88,12 +65,12 @@ class PlaybackThread(threading.Thread):
         # to optimize loop performance, dereference everything ahead of time
         
         filelist = self.filelist
-        playerlist = []
+        playerlist = [None,None ]
         idx = self.idx
         dt = self.dt
 
         gen_file(filelist[idx])
-        playerlist[idx] = sound.Player(filelist[idx])
+        playerlist[idx] = sound.Player(filelist[idx].name)
         
         tic = time.time
         while self.alive:
@@ -104,10 +81,20 @@ class PlaybackThread(threading.Thread):
             #2) generate
             gen_file(filelist[idx])
             #3) load
-            playerlist[idx] = sound.Player(filelist[idx])
+            p=playerlist[idx] = sound.Player(filelist[idx].name)
             #4) sleep
-            time.sleep(t0+dt-tic())
-
+            try:
+               time.sleep((t0+dt-tic())-0.005)
+               #time.sleep((p.duration-p.current_time))
+            except:
+                pass
+        self.cleanup()
+        
+    def cleanup(self):
+        try:
+            [x.close() for x in self.filelist]
+        except:
+            pass
     def stop(self):
         self.alive = False
 
@@ -122,8 +109,35 @@ class PlaybackThread(threading.Thread):
     def clear_wav_data(self):
         self.recording = []
 
-
+class Theramin(ui.View):
+    def __init__(self,dt):
+        self.t={}
+        self.dt=dt
+    def touch_began(self,touch):
+        self.t[touch.touch_id]=PlaybackThread(name="test",dt=self.dt)
+        (x,y)=touch.location
+        self.setfreq(touch.touch_id,y/self.height,x/self.width)
+        self.t[touch.touch_id].start()
+    def touch_moved(self,touch):
+        (x,y)=touch.location
+        self.setfreq(touch.touch_id,y/self.height,x/self.width)
+    def touch_ended(self,touch):
+        try:
+            self.t[touch.touch_id].stop()
+        except KeyError:
+            pass
+    def setfreq(self,touch_id,freq,vol):
+        freq=fmin+(fmax-fmin)*(1-freq)
+        self.t[touch_id].vol=vol
+        self.t[touch_id].ft=freq
+    def will_close(self):
+        for t in self.t.values():
+            try:
+                t.stop()
+            except:
+                pass
 if __name__ == '__main__':
-t=PlaybackThread(name="test")
-t.start()
+    v=Theramin(0.1)
+    v.present()
+
 
